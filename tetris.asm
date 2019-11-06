9816a828cc4b6951c5debbe8b710cfab00258b9e
lundwall <23060984+lundwall@users.noreply.github.com>
2019-11-06 08:40:29 +0100

    used template provided :(

--- tetris.asm
+++ tetris.asm
@@ -1,73 +1,322 @@
-;; tetris game
+  ;; game state memory location
+  .equ T_X, 0x1000                  ; falling tetrominoe position on x
+  .equ T_Y, 0x1004                  ; falling tetrominoe position on y
+  .equ T_type, 0x1008               ; falling tetrominoe type
+  .equ T_orientation, 0x100C        ; falling tetrominoe orientation
+  .equ SCORE,  0x1010               ; score
+  .equ GSA, 0x1014                  ; Game State Array starting address
+  .equ SEVEN_SEGS, 0x1198           ; 7-segment display addresses
+  .equ LEDS, 0x2000                 ; LED address
+  .equ RANDOM_NUM, 0x2010           ; Random number generator address
+  .equ BUTTONS, 0x2030              ; Buttons addresses
 
-;; game state memory location
-.equ T_X,           0x1000    ; falling tetromino position on x
-.equ T_Y,           0x1004    ; falling tetromino position on y
-.equ T_type,        0x1008    ; falling tetromino type
-.equ T_orientation, 0x100C    ; falling tetromino orientation
-.equ SCORE,         0x1010    ; score
-.equ GSA,           0x1014    ; Game State Array starting address
-.equ SEVEN_SEGS,    0x1198    ; 7-segment display addresses
-.equ LEDS,          0x2000    ; LED address
-.equ RANDOM_NUM,    0x2010    ; Random number generator address
-.equ BUTTONS,       0x2030    ; Buttons addresses
+  ;; type enumeration
+  .equ C, 0x00
+  .equ B, 0x01
+  .equ T, 0x02
+  .equ S, 0x03
+  .equ L, 0x04
 
-;; tetromino type enumeration
-.equ C, 0x00                  ; carre (square)
-.equ B, 0x01                  ; bar-shape
-.equ T, 0x02                  ; t-shape
-.equ S, 0x03                  ; s-shape
-.equ L, 0x04                  ; l-shape
+  ;; GSA type
+  .equ NOTHING, 0x0
+  .equ PLACED, 0x1
+  .equ FALLING, 0x2
 
-;; GSA type
-.equ NOTHING, 0x00			  ; the array location is not occupied
-.equ PLACED,  0x01			  ; occupied by a nonmoving object
-.equ FALLING, 0x02			  ; occupied by a falling object
+  ;; orientation enumeration
+  .equ N, 0
+  .equ E, 1
+  .equ So, 2
+  .equ W, 3
+  .equ ORIENTATION_END, 4
 
-;; orientation enumeration
-.equ N,  0x00				  ; north
-.equ E,  0x01				  ; east
-.equ So, 0x02				  ; south
-.equ W,  0x03				  ; west
+  ;; collision boundaries
+  .equ COL_X, 4
+  .equ COL_Y, 3
 
-;; rotation enumeration
-.equ CLOCKWISE, 0			  ; clockwise direction of rotation
-.equ COUNTERCLOCKWISE, 1	  ; counterclockwise direction of rotation
+  ;; Rotation enumeration
+  .equ CLOCKWISE, 0
+  .equ COUNTERCLOCKWISE, 1
 
-;; actions over tetrominoes
-.equ moveL, 0x01			  ; move left, horizontally
-.equ rotL,  0x02			  ; rotate counterclockwise
-.equ reset, 0x04			  ; reset the game
-.equ rotR,  0x08			  ; rotate clockwise
-.equ moveR, 0x10			  ; move right, horizontally
-.equ moveD, 0x20			  ; move down, vertically
+  ;; Button enumeration
+  .equ moveL, 0x01
+  .equ rotL, 0x02
+  .equ reset, 0x04
+  .equ rotR, 0x08
+  .equ moveR, 0x10
+  .equ moveD, 0x20
 
-;; collision return enum
-.equ W_COL,   0x00			  ; collision on the west side of tetrominoe
-.equ E_COL,   0x01			  ; collision on the east side of tetrominoe
-.equ So_COL,  0x02 			  ; collision on the south side of tetrominoe
-.equ OVERLAP, 0x03			  ; tetromino overlaps with something
-.equ NONE,    0x04			  ; tetromino does not collide with anything
+  ;; Collision return ENUM
+  .equ W_COL, 0
+  .equ E_COL, 1
+  .equ So_COL, 2
+  .equ OVERLAP, 3
+  .equ NONE, 4
 
-;; start location
-.equ START_X, 6				  ; start tetromino x-axis coordinate
-.equ START_Y, 1				  ; start tetromino y-axis coordinate
+  ;; start location
+  .equ START_X, 6
+  .equ START_Y, 1
 
-;; standard limits 
-.equ X_LIMIT, 12
-.equ Y_LIMIT, 8
+  ;; game rate of tetrominoe falling down (in terms of game loop iteration)
+  .equ RATE, 5
 
-;; game rate of tetrominoes falling down (in terms of game loop iteration)
-.equ RATE, 5
-
-
-
-
-
+  ;; standard limits
+  .equ X_LIMIT, 12
+  .equ Y_LIMIT, 8
 
 
+  ;; TODO Insert your code here
+font_data:
+    .word 0xFC  ; 0
+    .word 0x60  ; 1
+    .word 0xDA  ; 2
+    .word 0xF2  ; 3
+    .word 0x66  ; 4
+    .word 0xB6  ; 5
+    .word 0xBE  ; 6
+    .word 0xE0  ; 7
+    .word 0xFE  ; 8
+    .word 0xF6  ; 9
 
+C_N_X:
+  .word 0x00
+  .word 0xFFFFFFFF
+  .word 0xFFFFFFFF
 
+C_N_Y:
+  .word 0xFFFFFFFF
+  .word 0x00
+  .word 0xFFFFFFFF
 
+C_E_X:
+  .word 0x01
+  .word 0x00
+  .word 0x01
 
+C_E_Y:
+  .word 0x00
+  .word 0xFFFFFFFF
+  .word 0xFFFFFFFF
 
+C_So_X:
+  .word 0x01
+  .word 0x00
+  .word 0x01
+
+C_So_Y:
+  .word 0x00
+  .word 0x01
+  .word 0x01
+
+C_W_X:
+  .word 0xFFFFFFFF
+  .word 0x00
+  .word 0xFFFFFFFF
+
+C_W_Y:
+  .word 0x00
+  .word 0x01
+  .word 0x01
+
+B_N_X:
+  .word 0xFFFFFFFF
+  .word 0x01
+  .word 0x02
+
+B_N_Y:
+  .word 0x00
+  .word 0x00
+  .word 0x00
+
+B_E_X:
+  .word 0x00
+  .word 0x00
+  .word 0x00
+
+B_E_Y:
+  .word 0xFFFFFFFF
+  .word 0x01
+  .word 0x02
+
+B_So_X:
+  .word 0xFFFFFFFE
+  .word 0xFFFFFFFF
+  .word 0x01
+
+B_So_Y:
+  .word 0x00
+  .word 0x00
+  .word 0x00
+
+B_W_X:
+  .word 0x00
+  .word 0x00
+  .word 0x00
+
+B_W_Y:
+  .word 0xFFFFFFFE
+  .word 0xFFFFFFFF
+  .word 0x01
+
+T_N_X:
+  .word 0xFFFFFFFF
+  .word 0x00
+  .word 0x01
+
+T_N_Y:
+  .word 0x00
+  .word 0xFFFFFFFF
+  .word 0x00
+
+T_E_X:
+  .word 0x00
+  .word 0x01
+  .word 0x00
+
+T_E_Y:
+  .word 0xFFFFFFFF
+  .word 0x00
+  .word 0x01
+
+T_So_X:
+  .word 0xFFFFFFFF
+  .word 0x00
+  .word 0x01
+
+T_So_Y:
+  .word 0x00
+  .word 0x01
+  .word 0x00
+
+T_W_X:
+  .word 0x00
+  .word 0xFFFFFFFF
+  .word 0x00
+
+T_W_Y:
+  .word 0xFFFFFFFF
+  .word 0x00
+  .word 0x01
+
+S_N_X:
+  .word 0xFFFFFFFF
+  .word 0x00
+  .word 0x01
+
+S_N_Y:
+  .word 0x00
+  .word 0xFFFFFFFF
+  .word 0xFFFFFFFF
+
+S_E_X:
+  .word 0x00
+  .word 0x01
+  .word 0x01
+
+S_E_Y:
+  .word 0xFFFFFFFF
+  .word 0x00
+  .word 0x01
+
+S_So_X:
+  .word 0x01
+  .word 0x00
+  .word 0xFFFFFFFF
+
+S_So_Y:
+  .word 0x00
+  .word 0x01
+  .word 0x01
+
+S_W_X:
+  .word 0x00
+  .word 0xFFFFFFFF
+  .word 0xFFFFFFFF
+
+S_W_Y:
+  .word 0x01
+  .word 0x00
+  .word 0xFFFFFFFF
+
+L_N_X:
+  .word 0xFFFFFFFF
+  .word 0x01
+  .word 0x01
+
+L_N_Y:
+  .word 0x00
+  .word 0x00
+  .word 0xFFFFFFFF
+
+L_E_X:
+  .word 0x00
+  .word 0x00
+  .word 0x01
+
+L_E_Y:
+  .word 0xFFFFFFFF
+  .word 0x01
+  .word 0x01
+
+L_So_X:
+  .word 0xFFFFFFFF
+  .word 0x01
+  .word 0xFFFFFFFF
+
+L_So_Y:
+  .word 0x00
+  .word 0x00
+  .word 0x01
+
+L_W_X:
+  .word 0x00
+  .word 0x00
+  .word 0xFFFFFFFF
+
+L_W_Y:
+  .word 0x01
+  .word 0xFFFFFFFF
+  .word 0xFFFFFFFF
+
+DRAW_Ax:                        ; address of shape arrays, x axis
+    .word C_N_X
+    .word C_E_X
+    .word C_So_X
+    .word C_W_X
+    .word B_N_X
+    .word B_E_X
+    .word B_So_X
+    .word B_W_X
+    .word T_N_X
+    .word T_E_X
+    .word T_So_X
+    .word T_W_X
+    .word S_N_X
+    .word S_E_X
+    .word S_So_X
+    .word S_W_X
+    .word L_N_X
+    .word L_E_X
+    .word L_So_X
+    .word L_W_X
+
+DRAW_Ay:                        ; address of shape arrays, y_axis
+    .word C_N_Y
+    .word C_E_Y
+    .word C_So_Y
+    .word C_W_Y
+    .word B_N_Y
+    .word B_E_Y
+    .word B_So_Y
+    .word B_W_Y
+    .word T_N_Y
+    .word T_E_Y
+    .word T_So_Y
+    .word T_W_Y
+    .word S_N_Y
+    .word S_E_Y
+    .word S_So_Y
+    .word S_W_Y
+    .word L_N_Y
+    .word L_E_Y
+    .word L_So_Y
+    .word L_W_Y
